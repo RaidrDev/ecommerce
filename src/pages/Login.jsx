b850f7d4d5c2a2ed6cc2754a854b3e15eeee9066
Jose Sánchez <raidrdev@gmail.com>
2021-10-19 01:45:05 +0200

    ADD: login page & register page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255,255,255,0.5), 
+        rgba(255,255,255,0.5)
+    ),
+    url("https://images.pexels.com/photos/833052/pexels-photo-833052.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
+    center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+`
+
+const Title = styled.h1`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    font-weight: 300;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-top: 15px;
+`
+
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 12px;
+    text-decoration: underline;
+    text-align: center;
+    cursor: pointer;
+`
+
+const Login = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>LOG IN</Title>
+                <Form>
+                    <Input placeholder="username" />
+                    <Input type="password" placeholder="password" />
+                    <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+                    <Link>CREATE A NEW ACCOUNT</Link>
+                </Form>
+                <ButtonWrapper>
+                    <Button>LOGIN</Button>
+                </ButtonWrapper>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Login
